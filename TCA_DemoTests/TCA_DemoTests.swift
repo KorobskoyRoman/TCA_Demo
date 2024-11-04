71893f5050fa1e479a8fb53c15dedfc80e9330ac
Korobskoy_Roman <r.korobskoy@shinobi.software>
2024-11-04 13:28:05 +0300

    Initial Commit

---
+++ TCA_DemoTests/TCA_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_DemoTests.swift
+//  TCA_DemoTests
+//
+//  Created by Roman Korobskoy on 04.11.2024.
+//
+
+import Testing
+@testable import TCA_Demo
+
+struct TCA_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
